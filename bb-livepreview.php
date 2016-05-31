25bddc5a703d2a10708b11f6a12eddf46bf41f5e
badabingbreda <didou@badabing.nl>
2016-05-31 12:09:24 +0200

    Added checkbox to enable the preview and quicksave

--- bb-livepreview.php
+++ bb-livepreview.php
@@ -4,11 +4,11 @@
  * Plugin Name: Beaver Builder Live Preview and Quicksave
  * Plugin URI: http://www.badabing.nl
  * Description: By the press of a button view your layout as if you were on the frontend, not on the backend. Also adds a quicksave button, which publishes the layout with just one click.
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author: Badabing
  * Author URI: http://www.badabing.nl
   */
-define( 'BBLIVEPREVIEW_VERSION' , '1.0.1' );
+define( 'BBLIVEPREVIEW_VERSION' , '1.1.0' );
 define( 'BBLIVEPREVIEW_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BBLIVEPREVIEW_URL', plugins_url( '/', __FILE__ ) );
 
--- livepreview/includes/livepreview.js
+++ livepreview/includes/livepreview.js
@@ -3,7 +3,7 @@
 	$( document ).ready ( function () {
 
 
-		function toggle_fullview() {
+		function toggle_livepreview() {
 			if ( !isfullview ) {
 				FLBuilder._destroyOverlayEvents();
 				$('.fl-builder-bar-content').css('display' , 'none');
@@ -15,18 +15,37 @@
 				$('html.fl-builder-edit').prop('style' , '' );
 				$('.fl-col-highlight .fl-col-content').prop('style', '');
 			}
-			$('#bbshowfull a').toggleClass('full');
+			$('#bblivepreview a').toggleClass('preview');
 			FLBuilder._closePanel();
 			isfullview = !isfullview;
 		}
 
+		toggle_showlivepreview = function () {
+			var ischecked = $('#butbblivepreview').prop('checked');
+			Cookies.set('bbshowlivepreview', ischecked );
+			if (ischecked) { $( '#bblivepreview' ).removeClass('hidden'); } else { $( '#bblivepreview' ).addClass('hidden'); }
+		}
+		toggle_showquicksave = function () {
+			var ischecked = $('#butbbquicksave').prop('checked');
+			Cookies.set('bbshowquicksave', ischecked );
+			if (ischecked) { $( '#bbquicksave' ).removeClass('hidden'); } else { $( '#bbquicksave' ).addClass('hidden'); }
+		}
+
 		if ( typeof FLBuilderModel != undefined ) {
 			var isfullview = false;
-			$('<div id="bbshowfull"><a href="javascript:void(0);" title="Hide Editor"></a></div>').prependTo('.fl-page');
-			$('<div id="bbquicksave"><a href="javascript:void(0);" title="Quicksave"></a></div>').prependTo('.fl-page');
+			$('<div id="bblivepreview"><a href="javascript:void(0);" title="Hide Editor"></a></div>').prependTo('.fl-page');
+			$('<div class="fl-builder-panel-info fl-builder-blocks-section"><div class="fl-builder-blocks-section-title"><input type="checkbox" value="1" name="butbblivepreview" id="butbblivepreview" '+((Cookies.get('bbshowlivepreview')=='true')?'checked':'')+'><label for="butbblivepreview">Show Live Preview</label></div></div>').appendTo('div.fl-builder-panel-content');
 
-			$('.fl-page').delegate('#bbshowfull', 'click', toggle_fullview );
+			$('<div id="bbquicksave"><a href="javascript:void(0);" title="Quicksave"></a></div>').prependTo('.fl-page');
+			$('<div class="fl-builder-panel-info fl-builder-blocks-section"><div class="fl-builder-blocks-section-title"><input type="checkbox" value="1" name="butbbquicksave" id="butbbquicksave" '+((Cookies.get('bbshowquicksave')=='true')?'checked':'')+'><label for="butbbquicksave">Show Quicksave button</label></div></div>').appendTo('div.fl-builder-panel-content');
+
+			$('.fl-page').delegate('#bblivepreview', 'click', toggle_livepreview );
 			$('.fl-page').delegate('#bbquicksave' , 'click' , FLBuilder._publishButtonClicked );
+			$('body').delegate('#butbblivepreview' , 'click' , toggle_showlivepreview );
+			$('body').delegate('#butbbquicksave' , 'click' , toggle_showquicksave );
+
+			toggle_showlivepreview();
+			toggle_showquicksave();
 		}
 	});
 
